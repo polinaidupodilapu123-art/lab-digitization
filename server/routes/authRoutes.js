290f4a71057dd1b98dca8e1e8f4e93e9dbc43492
polinaidupodilapu123-art <polinaidupodilapu123@gmail.com>
2026-05-26 09:22:23 +0530

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-router.post('/login', authController.login);
-router.post('/student-setup', authController.studentSetup);
-router.get('/fix-admin', authController.fixAdmin);
-
-module.exports = router;
